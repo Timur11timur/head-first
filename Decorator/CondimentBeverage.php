e7e585bff8d4c0302732d12f5c8a8820aada4a59
Timur11timur <timur__timur@mail.ru>
2021-08-10 09:25:54 +0300

    Add constructor to CondimentBeverage

--- Decorator/CondimentBeverage.php
+++ Decorator/CondimentBeverage.php
@@ -4,5 +4,10 @@ namespace App\Decorator;
 
 abstract class CondimentBeverage extends Beverage
 {
+    protected Beverage $beverage;
 
+    public function __construct(Beverage $beverage)
+    {
+        $this->beverage = $beverage;
+    }
 }
--- Decorator/Condiments/Whip.php
+++ Decorator/Condiments/Whip.php
@@ -2,18 +2,10 @@
 
 namespace App\Decorator\Condiments;
 
-use App\Decorator\Beverage;
 use App\Decorator\CondimentBeverage;
 
 class Whip extends CondimentBeverage
 {
-    private Beverage $beverage;
-
-    public function __construct(Beverage $beverage)
-    {
-        $this->beverage = $beverage;
-    }
-
     public function getDescription(): string
     {
         return $this->beverage->getDescription() . ', Whip';
